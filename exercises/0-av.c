5647821c125eedf0c71c3e6eb0fdce96dc1ba545
Priya <pvaidya56@gmail.com>
2018-11-09 02:17:47 +0000

    Initial commit

---
+++ exercises/0-av.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - AV
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	int i;
+	for(i = 1; i < ac; i++)
+	{
+		printf("%s\n", av[i]);
+	}
+	return (0);
+}
