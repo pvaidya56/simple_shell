494fffeec7d63821750ad92c9083669b03633ec8
Priya <pvaidya56@gmail.com>
2018-11-17 23:32:56 +0000

    finished execve

--- shell.c/main.c
+++ shell.c/main.c
@@ -1,25 +1,46 @@
 #include "shell.h"
+/**
+ *  main - Main shell function
+ *
+ *
+ *Return - Always 0.
+ */
 
-int main (void)
+int main(void)
 {
 	int status;
 	pid_t child;
 	char *buffer;
 	size_t buffsize = 0;
-	char *token[1024];
+	char *split[1024];
 	size_t letters;
+	int index;
 
-	while ()
+	while (1)
 	{
 		child = fork();
 		if (child == -1)
 			return (-1);
 		if (child == 0)
 		{
-			write(STDIN
+			write(STDIN_FILENO, "$ ", 2);
+			letters = (getline(&buffer, &buffsize, 1));
+			if (letters == -1)
+				return (-1);
+			if ((buffer)[letters - 1] == '\n')
+			{
+				(buffer)[letters - 1] = '\0';
+			}
+			token = strtok(buffer, " ");
+			while (split[index] != NULL)
+			{
+				split[index] = strtok(NULL, " ");
+				index++;
+			}
+			execve(split[0], split, NULL);
+			perror("Execution Error");
+			return (-1);
 		}
-
-
 		if (child > 0)
 			wait(&status);
 	}
