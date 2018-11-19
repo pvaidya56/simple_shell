60b16bb1180403e39de23d46345b713c6abe4c10
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-11-19 20:02:35 +0000

    main file

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include "shell.h"
 /**
  *main - entry
- *
+ *@argc: argument count
+ *@argv: argument
+ *@env: implementation env
+ *Return: 0 for success
  */
 int main(int argc, char **argv, char **env)
 {
-	if (argc == 1 || argv[1] == NULL)
-		_loop(env);
+	(void)argc;
+	(void)argv;
+	_loop(env);
 	return (0);
 }
