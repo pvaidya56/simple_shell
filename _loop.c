0502ac6ee7b2857d58130717c91984643faecee3
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-11-19 19:53:08 +0000

    the infinite loop

--- _loop.c
+++ _loop.c
@@ -1,7 +1,8 @@
 #include "shell.h"
 /**
  *_loop - entry point
- *
+ *Description: infinite loop waiting for the command
+ *@env: loop with no command, or if needed, command is env
  */
 void _loop(char **env)
 {
@@ -20,15 +21,6 @@ void _loop(char **env)
 			continue;
 		}
 		args = split_line(line);
-
-/**		if (access(args[0], F_OK) != 0 && access(args[0], X_OK) != 0)
-			combine = get_PATH_token(args[0]);
-		printf("command is %s\n", args[0]);
-		printf("combine is %s\n", combine);
-		if (combine)
-			args[0] = combine;
-		printf("command after change is %s\n", args[0]);
-		launch = execute(args); */
 		directory = dir_only(env);
 		combine = check_path(directory, args[0]);
 		built_in_check(combine, args);
